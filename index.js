331e16c8318330484a41ef378c8a6d3f2edb011f
Alex S. <alex.santexis@gmail.com>
2024-03-15 09:09:26 +0200

    Initialize

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI).then(
+  () => {
+    console.log("Connection to mongodb established");
+  },
+  (err) => {
+    console.log("Failed to connect to mongodb", err);
+  }
+);
+
+const user = require("./route/user.route");
+const userProduct = require("./route/user.product.routes");
+
+app.use("/api/users", user);
+app.use("/api/user-products", userProduct);
+
+app.listen(port, () => {
+  console.log("Server is up");
+});
